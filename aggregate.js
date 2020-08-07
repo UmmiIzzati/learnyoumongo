15e9df0c5774bfa1f0e3dad0be03fa8176fddd3a
Ummi Izzati <ummizati95@gmail.com>
2020-08-07 17:05:52 +0800

    first commit

---
+++ aggregate.js
@@ -0,0 +1,25 @@
+var mongo = require('mongodb').MongoClient;
+var url = 'mongodb://localhost:27017/learnyoumongo';
+var size = process.argv[2];
+
+mongo.connect(url, function(err, db) {
+	var database = db.db('learnyoumongo')
+
+	database.collection('prices').aggregate([
+				{ $match: { size: size } },
+				{ $group: {_id: 'average',
+					   total: { $avg: '$price' } 
+				} }])
+		       .toArray(function(err, results) {
+				if (err) {
+					console.log(err);
+				}
+				if (!results.length) {
+					throw new Error('No results found')
+				}
+				console.log(Number(results[0].total).toFixed(2));
+			});
+
+	db.close();
+
+});
