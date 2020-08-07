194a85f4d4f2d8ebf549e8bba646bd551f0c467f
Ummi Izzati <ummizati95@gmail.com>
2020-08-07 11:51:54 +0800

    first commit

---
+++ remove.js
@@ -0,0 +1,21 @@
+var mongo = require('mongodb').MongoClient;
+var url = 'mongodb://localhost:27017/learnyoumongo';
+
+mongo.connect(url, function(err, db) {
+  if (err) {
+		console.log('Error connecting');
+	}
+
+  var database = db.db(process.argv[2]);
+
+  var id = process.argv[4]
+
+  database.collection(process.argv[3]).remove({ _id: id }
+  	, function(err, data) {
+    if (err) {
+			console.log('Error reading');
+		}
+    console.log(JSON.stringify(myObj));
+    db.close();
+  });
+});
